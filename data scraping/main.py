bdf1bf24f65bdb31390f1a8964ec00c3571f392d
Ahmed said <ahmedsaid2000321@gmail.com>
2023-05-30 19:24:53 +0300

    data scraping

--- data scraping/main.py
+++ data scraping/main.py
@@ -1,4 +1,20 @@
-import httpx
+"""
+* This script scrapes mobile phone data from the Mob4me website for a given list of mobile phone brands.
+* The scraped data includes :
+    the phone's name,
+    brand,
+    front and back camera specs,
+    screen size,
+    RAM,
+    memory,
+    battery,
+    and price.
+
+* The script uses the requests library to make HTTP requests to the Mob4me website and the BeautifulSoup library to parse the HTML content of the website.
+* The scraped data is stored in a pandas DataFrame and saved to an Excel file.
+"""
+
+import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
@@ -14,33 +30,35 @@ from consts import (
     MEMORY_NAME_SELECTOR,
     NEXT_BUTTON_SELECTOR,
     PHONE_ANCHORS_SELECTOR,
-    brands
+    BRANDS
 )
 
-def get_data_of_phone(phone_url):
-    response = httpx.get(phone_url, headers=headers)
-    soup = BeautifulSoup(response.content, 'lxml')
-    name = phone_url.split('.com/')[-1].replace('-',' ')
-    back_cam = soup.select_one(BACK_CAM_SELECTOR).text.replace('ميجا بكسل','').strip()
-    screen_size = soup.select_one(SCREEN_SIZE_SELECTOR)
-    battery = soup.select_one(BATTERY_SELECTOR).text
-    price = soup.select_one(PRICE_SELECTOR).text
+def get_element_text(soup, selector):
+    element = soup.select_one(selector).text if soup.select_one(selector) else "Not Found"
+    return element
 
-    memory_name = soup.select_one(MEMORY_NAME_SELECTOR)
-    if memory_name:
-        memory = memory_name.find_next('td').text.replace('\n','').strip()
+
+def get_next_element_text(soup, selector):
+    element = soup.select_one(selector) if soup.select_one(selector) else "Not Found"
+    if element == "Not Found":
+        return "Not Found"
     else:
-        memory = "Not Found"
-    ram_name = soup.select_one(RAM_NAME_SELECTOR)
-    if ram_name:
-        ram = ram_name.find_next('td').text.replace('\n','').strip()
-    else:
-        ram = "Not Found"
-    front_cam_name = soup.select_one(FRONT_CAM_SELECTOR)
-    if front_cam_name:
-        front_cam = front_cam_name.find_next('td').text.replace('\n','').strip()
-    else:
-        front_cam = "Not Found"
+        return element.find_next('td').text.replace('\n','').strip()
+
+
+def get_data_of_phone(phone_url):
+    response = requests.get(phone_url, headers=headers)
+    soup = BeautifulSoup(response.content, 'lxml')
+
+    name = phone_url.split('.com/')[-1].replace('-',' ')
+    back_cam = get_element_text(soup,BACK_CAM_SELECTOR).replace('ميجا بكسل','').strip()
+    screen_size = get_element_text(soup,SCREEN_SIZE_SELECTOR) 
+    battery = get_element_text(soup,BATTERY_SELECTOR)  
+    price = get_element_text(soup,PRICE_SELECTOR)  
+
+    memory = get_next_element_text(soup, MEMORY_NAME_SELECTOR)
+    ram = get_next_element_text(soup, RAM_NAME_SELECTOR)
+    front_cam = get_next_element_text(soup, FRONT_CAM_SELECTOR)
 
     return {
         'name':name,
@@ -55,10 +73,11 @@ def get_data_of_phone(phone_url):
         'price':price
     }
 
+
 def get_data_of_page(brand , page):
     page_data = pd.DataFrame()
     page_url = f"{URL}{brand}/{page}"
-    response = httpx.get(page_url,headers=headers)
+    response = requests.get(page_url,headers=headers)
     soup = BeautifulSoup(response.content, 'lxml')
     phone_anchors = soup.select(PHONE_ANCHORS_SELECTOR)
     for a in phone_anchors:
@@ -72,24 +91,30 @@ def get_data_of_page(brand , page):
         
 
 
+
 def scrap(brand):
     page = 1
     phones_data,next_button_is_active = get_data_of_page(brand, page)
     while next_button_is_active:
         page+=1
+        print(brand, page)
         page_data,next_button_is_active = get_data_of_page(brand, page)
         phones_data = pd.concat([phones_data, page_data])
+        if page == 5:
+            break # the latest 5 pages will contan latest 100 mobile of each brand 
     return phones_data   
 
-def save_data_to_csv(dataframe, file_name):
-    dataframe.to_csv(file_name, index=False)
+
+def save_data_to_excel(dataframe, file_name):
+    dataframe.to_excel(file_name, index=False)
+
 
 def main():
     all_brands_data = pd.DataFrame()
-    for brand in brands:
+    for brand in BRANDS:
         brand_data = scrap(brand)
         all_brands_data = pd.concat([all_brands_data,brand_data])
-        save_data_to_csv(all_brands_data,"mob4me.csv")
+        save_data_to_excel(all_brands_data,"mob4me.xlsx")
 
 if __name__ == '__main__':
     main()
